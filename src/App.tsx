4f601fe35049ac5b74fd49f37134823939e93f05
sayache-nova <salim.ayache@nova-technology.fr>
2022-12-16 01:09:03 +0100

    ready to push

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import { FC, useEffect, useState } from "react";
 
-function App() {
+import Post from "./components/posts.component";
+import User from "./components/users.component";
+import { PostModel, UserModel } from "./models";
+
+const API = {
+  POSTS: "https://jsonplaceholder.typicode.com/posts",
+  USERS: "https://jsonplaceholder.typicode.com/users",
+};
+
+const App: FC = () => {
+  console.log("rendered");
+
+  const [activeLink, setActiveLink] = useState<"posts" | "users">("posts");
+  const [posts, setPosts] = useState<PostModel[] | null>(null);
+  const [users, setUsers] = useState<UserModel[] | null>(null);
+
+  useEffect(() => {
+    if (activeLink === "posts") {
+      fetch(API.POSTS, {
+        headers: { "Content-Type": "application/json" },
+      })
+        .then((data) => data.json())
+        .then((posts) => setPosts(posts))
+        .catch((err) => console.log(err));
+    } else {
+      fetch(API.USERS, {
+        headers: { "Content-Type": "application/json" },
+      })
+        .then((data) => data.json())
+        .then((users) => setUsers(users))
+        .catch((err) => console.log(err));
+    }
+  }, [activeLink]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+    <>
+      <nav>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          onClick={() => setActiveLink("posts")}
+          className={activeLink === "posts" ? "active" : undefined}
         >
-          Learn React
+          Posts
         </a>
-      </header>
-    </div>
+        <a
+          onClick={() => setActiveLink("users")}
+          className={activeLink === "users" ? "active" : undefined}
+        >
+          Users
+        </a>
+      </nav>
+
+      <main>
+        {activeLink === "posts"
+          ? posts?.map((post) => <Post key={post.id} data={post} />)
+          : users?.map((user) => <User key={user.id} data={user} />)}
+      </main>
+    </>
   );
-}
+};
 
 export default App;
